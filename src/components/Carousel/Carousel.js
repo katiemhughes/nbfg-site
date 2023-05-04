618b665996e52ebd8e2c3a9e236c6293b65a6b7f
katiemhughes <katie.m.hughes@hotmail.co.uk>
2023-05-04 20:49:54 +0100

    add mobile first styles

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,28 +9,20 @@ import Loader from '../Loader/Loader';
 import { Context } from '../../Context/Context';
 
 const Carousel = () => {
-    const { isCarouselLoading, carouselSlides, isMobileCarouselLoading, mobileCarouselSlides } = useContext(Context);
+    const { isCarouselLoading, carouselSlides } = useContext(Context);
     // console.log('carousel slides', carouselSlides);
-    if (isMobileCarouselLoading || isCarouselLoading) {
+    if (isCarouselLoading) {
         return <Loader />
     }
 
     // If there are no slides to display, then do not render the component.
-    if (!Array.isArray(carouselSlides) || (!Array.isArray(mobileCarouselSlides)) || (!carouselSlides.length || !mobileCarouselSlides.length)) {
+    if (!Array.isArray(carouselSlides) || !carouselSlides.length) {
         return null;
     }
 
   return (
     <div className='carousel'>
-        <Swiper autoHeight={true} modules={[Navigation]} navigation>
-            {mobileCarouselSlides.map((item) => {
-                const { mobileId, mobileSlideTitle, mobileSlideBgImage, mobileSlideItem, mobileSlideDescription } = item;
-                return (
-                    <SwiperSlide key={mobileId}>
-                        <CarouselSlide slideTitle={mobileSlideTitle} slideDescription={mobileSlideDescription} slideBgImage={slideBgImage} />
-                    </SwiperSlide>
-                )
-            }}
+        <Swiper modules={[Navigation]} navigation>
             {carouselSlides.map((item) => {
                 const { id, slideBgImage, slideTitle, slideDescription } = item;
                 return (
