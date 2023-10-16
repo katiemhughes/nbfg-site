ae9f87fe898e9480a3c9f881c60ae000ddf07eb5
katiemhughes <katie.m.hughes@hotmail.co.uk>
2023-10-16 19:26:42 +0100

    reenable under construction page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import { Provider } from './Context/Context';
-// import About from './components/About/About';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import Navbar from './components/Navbar/Navbar';
-// import Home from './components/Home/Home';
 import UnderConstruction from './components/UnderConstruction/UnderConstruction';
 
 const App = () => {
   return (
     <Provider>
       <BrowserRouter>
-      <UnderConstruction />
-        {/* <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes> */}
+        <UnderConstruction />
       </BrowserRouter>
     </Provider>
   );
