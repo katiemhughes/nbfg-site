bc8724e5a3c67bc504fcd9e851c70851cf7b8e83
katiemhughes <katie.m.hughes@hotmail.co.uk>
2023-12-27 18:19:05 +0000

    fix merge conflicts

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carousel from '../Carousel/Carousel';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
+// import Enter from '../Enter/Enter';
 
-function Home() {
+function Home({ enter, setEnter }) {
+  useEffect(() => {
+    setTimeout(() => {
+      setEnter(true);
+    }, 5000);
+  }, []);
+
   return (
     <div id="home">
+      <div>Enter video</div>
+      <a href="/">Enter link</a>
+      {/* <Enter /> */}
       <Carousel />
       <About />
       <Contact />
