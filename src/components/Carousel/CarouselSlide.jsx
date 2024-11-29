fe05cb391794fb6afc4d2302ad2ad4c2d47dc0f4
katiemhughes <katie.m.hughes@hotmail.co.uk>
2024-11-29 18:31:19 +0000

    Conditionally render mobile or desktop assets

--- src/components/Carousel/CarouselSlide.jsx
+++ src/components/Carousel/CarouselSlide.jsx
@@ -10,24 +10,25 @@ function CarouselSlide({
         <h2>{slideTitle}</h2>
         <p>{slideDescription}</p>
         <picture>
-          {slideBgImageTest && slideBgImageTest.map((image, index) => {
+          {slideBgImageTest && slideBgImageTest.filter((image) => !/mobile/i.test(image?.title)).map((image, index) => {
             const url = image?.file?.url;
-            const width = image?.file?.details?.image?.width;
-
-            if (!url || !width) {
-              return null;
-            }
 
             return (
               <source
                 key={url || index}
                 srcSet={url}
-                media={width > 768 ? '(min-width:800px)' : '(max-width:799px)'}
+                media="(min-width:768px)"
               />
             );
           })}
+          {slideBgImageTest && slideBgImageTest.filter((image) => /mobile/i.test(image?.title)).map((image, index) => {
+            const url = image?.file?.url;
+
+            return (
+              <img key={url || index} src={url} alt={slideTitle} className="content__image" />
+            );
+          })}
         </picture>
-        <img src={slideBgImageTest[1].file.url} alt={slideTitle} className="content__image" />
         <Link to={`/news/articles/${slideSlug}`} className="content__btn">Learn More</Link>
       </div>
     </div>
