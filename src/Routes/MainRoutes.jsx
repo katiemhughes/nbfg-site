82a9652a917c8726cca8acdf210a33292ff8d44e
katiemhughes <katie.m.hughes@hotmail.co.uk>
2024-12-02 16:01:54 +0000

    Make footer appear on all pages

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from '../components/NotFound/NotFound';
 import Newspage from '../components/News/Newspage';
@@ -6,8 +6,29 @@ import NewsList from '../components/News/NewsList';
 import Navbar from '../components/Navbar/Navbar';
 import Home from '../components/Home/Home';
 import GangMember from '../components/MeetTheGang/GangMember';
+import Contact from '../components/Contact/Contact';
 
 function MainRoutes() {
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar');
+    const root = document.documentElement;
+
+    if (navbar) {
+      root.style.setProperty('--navbar-height', `${navbar.offsetHeight}px`);
+    }
+
+    const handleResize = () => {
+      if (navbar) {
+        root.style.setProperty('--navbar-height', `${navbar.offsetHeight}px`);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <>
       <nav className="navbar">
@@ -20,6 +41,9 @@ function MainRoutes() {
         <Route path="/news/articles/:newsArticle" element={<Newspage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <footer className="footer">
+        <Contact />
+      </footer>
     </>
   );
 }
